c3724fc460b6b94be7d51e694698f204f923ddad
thomaz <thomazbastiaansser@gmail.com>
2023-06-17 23:47:15 +0100

    Setting up a repository to work on argonaut

---
+++ R/allClasses.R
@@ -0,0 +1,7 @@
+#' An S4 class stratifiedFeatureTable. 
+#'
+#' @description This is a specialized S4 class to represent stratified feature data.
+#'
+setClass("stratifiedFeatureTable",
+        contains = "array"
+)
