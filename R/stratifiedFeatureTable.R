1d88c275cbf1191eccf59761a5ad6ae1fe6db20c
Thomaz Bastiaanssen <thomazbastiaanssen@gmail.com>
2023-07-07 15:35:12 +0100

    sped up as.sft

--- R/stratifiedFeatureTable.R
+++ R/stratifiedFeatureTable.R
@@ -17,8 +17,8 @@ as.stratifiedFeatureTable <- function(x, sep = "\\|") {
   x = as.matrix(x)[order(row.names(x)),]
   
   dimlist <- list(
-    features = unique(sub(pattern = paste0(".*", sep), replacement = "", x = row.names(x))),
     samples  = colnames(x),
+    features = sort(unique(sub(pattern = paste0(".*", sep), replacement = "", x = row.names(x)))),
     subtypes = unique(sub(pattern = paste0(sep, ".*"), replacement = "", x = row.names(x)))
   )
   
@@ -27,81 +27,24 @@ as.stratifiedFeatureTable <- function(x, sep = "\\|") {
   ar = array(dim  = sapply(FUN = length, dimlist), 
              dimnames = dimlist
   )
-  
-  
-  
-  subtype_end   = cumsum(table(sub(pattern = paste0(sep, ".*"), replacement = "", x = row.names(x))))
-  subtype_begin = c(0, subtype_end[-dim(ar)[3]])+1
-  
-  feature_ind   = list()
-  
-  #This loop may be cleaned up
-  for(st in 1:dim(ar)[3]){
-    feature_ind[[st]] = which(unique(sub(pattern = paste0(".*", sep), 
-                                              replacement = "", 
-                                              x = row.names(x))) %in% 
-                                sub(pattern = paste0(".*", sep), 
-                                         replacement = "", 
-                                         row.names(x[subtype_begin[st]:subtype_end[st],])))
-  }
-  
-  #print("Starting for loop")
-  for(st in 1:dim(ar)[3]){
-    
-    ar[feature_ind[[st]],rep(T, dim(ar)[2]),st] <- x[subtype_begin[st]:subtype_end[st],, drop = FALSE]
-    #print(paste("finished round", st))
-  }
-  
-  #Return stratifiedFeatureTable
-  new("stratifiedFeatureTable", ar)
+
+  #Where in the array to put
+ar_ind = split(
+  x = as.integer(factor(sub(pattern = paste0(sep, ".*"), replacement = "", x = row.names(x)))),
+  f = sub(pattern = paste0(".*", sep), replacement = "", x = row.names(x))
+  )
+
+#Where in the matrix to take from
+x_ind = split(
+  x = 1:nrow(x),
+  f = sub(pattern = paste0(".*", sep), replacement = "", x = row.names(x))
+)
+
+#put it there 
+for(ft in 1:dim(ar)[2]){
+  ar[,ft,ar_ind[[ft]]] <- x[x_ind[[ft]],, drop = FALSE]
+  #print(paste("finished round", st))
 }
-
-
-#' Generate a stratified feature table object
-#'
-#' @description
-#' `as.stratifiedFeatureTable()` wrangles an existing table, such as a data frame, 
-#' into the stratifiedFeatureTable format. 
-#'
-#' @param x A table to be formatted to `stratifiedFeatureTable` format.
-#' @param sep A character string to separate the terms. 
-
-#' @export
-#' @examples
-#' sdf <- dummy_strat(10, 20, 10)
-#' 
-#' sft <- as.stratifiedFeatureTable(sdf)
-as.stratifiedFeatureTable2 <- function(x, sep = "\\|") {
-  
-  x = as.matrix(x)[order(row.names(x)),]
-  
-  dimlist <- list(
-    features = unique(sub(pattern = paste0(".*", sep), replacement = "", x = row.names(x))),
-    samples  = colnames(x),
-    subtypes = unique(sub(pattern = paste0(sep, ".*"), replacement = "", x = row.names(x)))
-  )
-  
-  
-  #Make container
-  ar = array(dim  = sapply(FUN = length, dimlist), 
-             dimnames = dimlist
-  )
-  
-  
-  
-  subtype_end   = cumsum(table(sub(pattern = paste0(sep, ".*"), replacement = "", x = row.names(x))))
-  subtype_begin = c(0, subtype_end[-dim(ar)[3]])+1
-  
-  feature_ind   = split(
-    x = as.integer(factor(sub(pattern = paste0(".*", sep), replacement = "", x = row.names(x)))), 
-    f = cumsum(!duplicated(sub(pattern = paste0(sep, ".*"), replacement = "", x = row.names(x))))
-    )
-  
-    for(st in 1:dim(ar)[3]){
-    
-    ar[feature_ind[[st]],rep(T, dim(ar)[2]),st] <- x[subtype_begin[st]:subtype_end[st],, drop = FALSE]
-    #print(paste("finished round", st))
-  }
   
   #Return stratifiedFeatureTable
   new("stratifiedFeatureTable", ar)
