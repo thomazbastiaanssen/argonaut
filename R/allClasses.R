5898943c3a636f571c3c60aa89691f8ac443624e
Thomaz Bastiaanssen <thomazbastiaanssen@gmail.com>
2023-10-03 18:28:54 +0100

    exports sft

--- R/allClasses.R
+++ R/allClasses.R
@@ -1,7 +1,7 @@
 #' An S4 class stratifiedFeatureTable. 
 #'
 #' @description This is a specialized S4 class to represent stratified feature data.
-#'
+#' @exportClass  stratifiedFeatureTable
 setClass("stratifiedFeatureTable",
         contains = "array"
 )
