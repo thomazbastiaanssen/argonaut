0d16a72924d91deeda19645e2f0b24d35e8322f0
Thomaz Bastiaanssen <thomazbastiaanssen@gmail.com>
2023-10-04 12:49:00 +0100

    update

--- R/allClasses.R
+++ R/allClasses.R
@@ -7,14 +7,6 @@ setClass("stratifiedFeatureTable",
         contains = "array"
 )
 
-setValidity("stratifiedFeatureTable", function(object) {
-  if (!identical(names(dimnames(object)), c("samples","features","subtypes"))) {
-    'dimnames must be exactly c("samples","features","subtypes").' 
-  } else {
-    TRUE
-  }
-})
-
 #' Make a suitably shaped array into a stratifiedFeatureTable. 
 #'
 #' @description Make a stratifiedFeatureTable, a specialized S4 class to represent stratified feature data.
@@ -30,3 +22,10 @@ stratifiedFeatureTable <- function(x) new("stratifiedFeatureTable", x)
 #' 
 sft <- stratifiedFeatureTable
 
+setValidity("stratifiedFeatureTable", method = function(object) {
+  if (!identical(names(dimnames(object)), c("samples","features","subtypes"))) {
+    'dimnames must be exactly c("samples","features","subtypes").' 
+  } else {
+    return(TRUE)
+  }
+})
