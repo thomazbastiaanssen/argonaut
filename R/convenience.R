3c5739570e3668d0562c1276d0e0ebebb989d34c
Thomaz Bastiaanssen <thomazbastiaanssen@gmail.com>
2023-10-05 12:51:05 +0100

    Implemented CLR in the case of hierarchical, subtype-dependent features (like in PICRUSt)

--- R/convenience.R
+++ R/convenience.R
@@ -46,3 +46,40 @@ apply_by <- function(X, MARGIN, FUN, na.exclude = TRUE, ..., simplify = TRUE){
 getFeature <- function(x, f){
   x[,f,][, !is.na(colSums(x[,f,]))]
 }
+
+#' Apply by dimension.
+#' @description A method to apply a function by dimension for stratified feature tables. Wraps around apply.  
+#' @param x A stratified feature table object to be CLR-transformed   
+#' @param dependence Placeholder. Whether feature levels are dependent on subtype levels (like with 16S-based inferential data). 
+#' @return A clr-transformed sft
+#' @importFrom deleuze getTableMeans
+#' @export
+#' 
+clr_sft <- function(x, dependence = TRUE){
+  if(dependence){
+    #collapse on subtype level and impute zeroes with MLE
+    x_mu <- deleuze::getTableMeans(apply_by(X = x, MARGIN = "features", FUN = mean), 
+                                   CLR_transformed = F, cols_as_features = T)
+    #close the compositions
+    x_mu = x_mu/rowSums(x_mu)
+    
+    #Compute log geometric mean per sample 
+    x_geom_mean = apply(x_mu, MARGIN = 1, FUN = function(x){mean(log(x))})
+    
+    #CLR transform 
+    x_imp  <- log(x_mu) - x_geom_mean
+    
+    #Make an array of zeroes and NAs
+    x_res = x * 0
+    
+    #Stack the CLR-transformed sample ~ subtype table with replicate and then reorient it
+    x_clr = aperm(a = replicate(n = dim(x)[2], x_imp), perm = c(1,3,2))
+    
+    stopifnot("Something went wrong: The dimensions of the resulting sft do not correspond to the input dimensions." = 
+                identical(unname(dim(x_clr)), unname(dim(x))))
+    
+    #Replace non-NA values, that were set to zero, with the CLR-transformed values for that subtype. 
+    x_res = sft(x_res + x_clr)
+    return(x_res)  
+  }
+}
